3e5354ab9b227257e2c37c6b14c4a0f8b8a22a90
dasom <dasom228@gmail.com>
2021-12-20 15:21:07 +0900

    Add getUser

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -3,14 +3,28 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
+func handleUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get users")
+}
+
+func handleGetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, "User ID : ", vars["id"])
+}
+
 func NewHttpHandler() http.Handler {
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
+	// mux := http.NewServeMux()
 	mux.HandleFunc("/", handleIndex)
+	mux.HandleFunc("/users", handleUsers)
+	mux.HandleFunc("/users/{id:[0-9]+}", handleGetUser)
 	return mux
 }
--- api/handler/handler_test.go
+++ api/handler/handler_test.go
@@ -25,3 +25,29 @@ func TestHandleIndex(t *testing.T) {
 	assert.Equal("Hello, world!", string(data))
 
 }
+
+func TestHandleUsers(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHttpHandler())
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/users")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Contains(string(data), "Get users")
+}
+
+func TestHandleGetUser(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHttpHandler())
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/users/58")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Contains(string(data), "58")
+}
