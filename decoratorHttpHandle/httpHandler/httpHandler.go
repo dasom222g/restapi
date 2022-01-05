0eae92022f9b721397102143af30bf589d3b55c2
dasom <dasom228@gmail.com>
2022-01-05 18:44:48 +0900

    Add page

--- decoratorHttpHandle/httpHandler/httpHandler.go
+++ decoratorHttpHandle/httpHandler/httpHandler.go
@@ -9,8 +9,13 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Users")
+}
+
 func NewHttpHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", IndexHandler)
+	mux.HandleFunc("/user", GetUsersHandler)
 	return mux
 }
