d7f30862a2047d2ca205daca6b83b4ec7beb82b6
dasom <dasom228@gmail.com>
2022-01-09 17:36:22 +0900

    Add template

--- template/main.go
+++ template/main.go
@@ -11,14 +11,27 @@ type User struct {
 	Age   int
 }
 
+func (u *User) IsOld() bool {
+	return u.Age > 30
+}
+
 func main() {
 	user1 := User{Name: "dasom", Email: "dasom@gmail.com", Age: 33}
 	user2 := User{Name: "kelly", Email: "kelly@gmail.com", Age: 11}
+	users := []User{user1, user2}
 	// struct key값이 대문자로 시작해야 템플릿에 값 주입 가능
-	tmpl, err := template.New("tmpl").Parse("Name: {{.Name}}\nEmail: {{.Email}}\nAge: {{.Age}}\n")
+	// tmpl, err := template.New("tmpl").Parse("Name: {{.Name}}\nEmail: {{.Email}}\nAge: {{.Age}}\n")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// tmpl.Execute(os.Stdout, user1)
+	// tmpl.Execute(os.Stdout, user2)
+
+	// 템플릿 파일로 관리
+	tmplFile, err := template.New("tmpl").ParseFiles("file/tmpl1.tmpl", "file/index.html")
 	if err != nil {
 		panic(err)
 	}
-	tmpl.Execute(os.Stdout, user1)
-	tmpl.Execute(os.Stdout, user2)
+	tmplFile.ExecuteTemplate(os.Stdout, "index.html", users)
+	// tmplFile.ExecuteTemplate(os.Stdout, "tmpl1.tmpl", user2)
 }
