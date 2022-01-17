0aa1fcb45f6a9bf069f08b33499275bef5067d89
dasom <dasom228@gmail.com>
2022-01-17 17:56:13 +0900

    Add eventSource

--- eventsource/public/chat.js
+++ eventsource/public/chat.js
@@ -1,1 +1,18 @@
-console.log('test!!')
+const formArea = document.querySelector('#chat-form')
+const nameArea = formArea.querySelector('.chat-name')
+const logArea = formArea.querySelector('.chat-log')
+
+const messageArea = formArea.querySelector('#chat-msg')
+const userName = prompt('What is your name?')
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const message = messageArea.value
+  messageArea.value = ''
+  messageArea.focus()
+
+  console.log('message', message, 'userName', userName)
+}
+
+
+formArea.addEventListener('submit', handleSubmit)
