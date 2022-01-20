d8b13f79747c1594123e04bfc16b68e0c110e477
dasom <dasom228@gmail.com>
2022-01-20 18:13:49 +0900

    Fix frontend

--- eventsource/public/chat.js
+++ eventsource/public/chat.js
@@ -1,18 +1,18 @@
 if (window.EventSource) {
-  const formArea = document.querySelector('#chat-form')
-  const formNameArea = document.querySelector('#user-form')
-  const nameArea = formArea.querySelector('.chat-name')
-  const logArea = formArea.querySelector('.chat-log')
+  const messageForm = document.querySelector('#chat-form')
+  const nameForm = document.querySelector('#user-form')
+  const nameArea = messageForm.querySelector('.chat-name')
+  const logArea = messageForm.querySelector('.chat-log')
   
-  const messageArea = formArea.querySelector('#chat-msg')
-  const username = prompt('What is your name?')
+  const messageInput = messageForm.querySelector('#chat-msg')
+  const nameInput = nameForm.querySelector('#username')
   let userInfo = {}
   
-  const handleSubmit = async (e) => {
+  const handleMessageSubmit = async (e) => {
     e.preventDefault()
     
     // 데이터 전송
-    const message = messageArea.value
+    const message = messageInput.value
     const {id, name} = userInfo
     const messageInfo = {
       id,
@@ -31,14 +31,15 @@ if (window.EventSource) {
     }
 
     // ui 초기화
-    messageArea.value = ''
-    messageArea.focus()
+    messageInput.value = ''
+    messageInput.focus()
   }
-  const handleUserSubmit = async (e) => {
+  const handleUserInfoSubmit = async (e) => {
     e.preventDefault()
-
+    const value = nameInput.value.trim()
+    if (!value.length) return
     try {
-      const data = { name: username }
+      const data = { name: value }
       const response = await fetch('/user', {
         method: 'POST',
         body: JSON.stringify(data),
@@ -52,19 +53,8 @@ if (window.EventSource) {
   }
   
   const es = new EventSource('/stream')
-  es.onopen = async () => {
-    try {
-      const data = { name: username }
-      const response = await fetch('/user', {
-        method: 'POST',
-        body: JSON.stringify(data),
-      })
-      const result = await response.json()
-      console.log('result', result)
-      userInfo = result
-    } catch(error) {
-      console.error(error)
-    }
+  es.onopen = () => {
+    console.log('eventSource open!!')
   }
 
   es.onmessage = (e) => {
@@ -73,6 +63,6 @@ if (window.EventSource) {
   }
   
   
-  formArea.addEventListener('submit', handleSubmit)
-  formNameArea.addEventListener('submit', handleUserSubmit)
+  messageForm.addEventListener('submit', handleMessageSubmit)
+  nameForm.addEventListener('submit', handleUserInfoSubmit)
 }
